f969c837e440a61997d98d362942d90c16bab7cc
Attila Szabo <attila.szabo@prezi.com>
2015-01-07 14:47:23 +0100

    AppRegistryNotReady fix. Use ugettext_lazy for translation.

--- vies/fields.py
+++ vies/fields.py
@@ -3,7 +3,7 @@ from __future__ import (unicode_literals, absolute_import)
 
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from . import VATIN, VIES_COUNTRY_CHOICES
 from .widgets import VATINWidget, VATINHiddenWidget
 
