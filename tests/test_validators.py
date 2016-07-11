07b4f3b74179447b8cec4a8e4bfd01e5892eeaaf
René Muhl <rene@transcode.de>
2016-07-11 15:23:28 +0200

    Improved existing tests

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -8,23 +8,38 @@ from vies.validators import VATINValidator
 
 class TestValidators(object):
     def test_valid(self):
+        valid_vatin = VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER)
         validator = VATINValidator()
-        vatin = VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER)
-        validator(vatin)
+        validator(valid_vatin)
 
         validator = VATINValidator(verify=False, validate=True)
-        vatin = VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER)
-        validator(vatin)
+        validator(valid_vatin)
 
         validator = VATINValidator(verify=True, validate=True)
-        vatin = VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER)
-        validator(vatin)
+        validator(valid_vatin)
 
     def test_no_check_exception(self):
         with pytest.raises(ValueError):
             VATINValidator(verify=False, validate=False)
 
     def test_invalid(self):
+        invalid_number_vatin = VATIN(VALID_VIES_COUNTRY_CODE, '12345678')
+        invalid_country_vatin = VATIN('XX', VALID_VIES_NUMBER)
+        validator = VATINValidator()
+
+        with pytest.raises(ValidationError):
+            validator(invalid_country_vatin)
+
+        validator = VATINValidator(verify=False, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
+
+        validator = VATINValidator(verify=True, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_country_vatin)
+
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
 
 
 class TestValidatorsWithoutVATINObject(object):
