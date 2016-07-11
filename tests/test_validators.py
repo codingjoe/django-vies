b3cf1abcfd49a3ff3cd7a6b24b497d8b3b32e47f
René Muhl <rene@transcode.de>
2016-07-11 11:49:17 +0200

    Added tests to check input of single char values

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -25,21 +25,36 @@ class TestValidators(object):
             VATINValidator(verify=False, validate=False)
 
     def test_invalid(self):
+
+
+class TestValidatorsWithoutVATINObject(object):
+    """Validate raw values without VATIN object from model field."""
+    def test_valid(self):
+        valid_vatin = ''.join([VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER])
         validator = VATINValidator()
-        vatin = VATIN('XX', VALID_VIES_NUMBER)
-        with pytest.raises(ValidationError):
-            validator(vatin)
+        validator(valid_vatin)
 
         validator = VATINValidator(verify=False, validate=True)
-        vatin = VATIN(VALID_VIES_COUNTRY_CODE, '12345678')
-        with pytest.raises(ValidationError):
-            validator(vatin)
+        validator(valid_vatin)
 
         validator = VATINValidator(verify=True, validate=True)
-        vatin = VATIN('XX', VALID_VIES_NUMBER)
-        with pytest.raises(ValidationError):
-            validator(vatin)
+        validator(valid_vatin)
 
-        vatin = VATIN(VALID_VIES_COUNTRY_CODE, '12345678')
+    def test_invalid(self):
+        invalid_number_vatin = ''.join([VALID_VIES_COUNTRY_CODE, '12345678'])
+        invalid_country_vatin = ''.join(['XX', VALID_VIES_NUMBER])
+
+        validator = VATINValidator()
         with pytest.raises(ValidationError):
-            validator(vatin)
+            validator(invalid_country_vatin)
+
+        validator = VATINValidator(verify=False, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
+
+        validator = VATINValidator(verify=True, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_country_vatin)
+
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
