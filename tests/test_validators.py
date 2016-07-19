25691cd5de1d3e863359d5ef1baed1b67aea2227
René Muhl <rene@transcode.de>
2016-07-19 17:06:58 +0200

    Parametrized validator tests

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -7,69 +7,45 @@ from vies.validators import VATINValidator
 
 
 class TestValidators(object):
-    def test_valid(self):
-        valid_vatin = VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER)
+    """Validate values with VATIN object and string values."""
+    @pytest.mark.parametrize('vatin', [
+        VATIN(VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER),
+        ''.join([VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER])
+    ])
+    def test_valid(self, vatin):
         validator = VATINValidator()
-        validator(valid_vatin)
+        validator(vatin)
 
         validator = VATINValidator(verify=False, validate=True)
-        validator(valid_vatin)
+        validator(vatin)
 
         validator = VATINValidator(verify=True, validate=True)
-        validator(valid_vatin)
+        validator(vatin)
+
+    @pytest.mark.parametrize('invalid_number_vatin', [
+        VATIN(VALID_VIES_COUNTRY_CODE, '12345678'),
+        ''.join([VALID_VIES_COUNTRY_CODE, '12345678'])
+    ])
+    @pytest.mark.parametrize('invalid_country_vatin', [
+        VATIN('XX', VALID_VIES_NUMBER),
+        ''.join(['XX', VALID_VIES_NUMBER])
+    ])
+    def test_invalid(self, invalid_number_vatin, invalid_country_vatin):
+        validator = VATINValidator()
+        with pytest.raises(ValidationError):
+            validator(invalid_country_vatin)
+
+        validator = VATINValidator(verify=False, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
+
+        validator = VATINValidator(verify=True, validate=True)
+        with pytest.raises(ValidationError):
+            validator(invalid_country_vatin)
+
+        with pytest.raises(ValidationError):
+            validator(invalid_number_vatin)
 
     def test_no_check_exception(self):
         with pytest.raises(ValueError):
             VATINValidator(verify=False, validate=False)
-
-    def test_invalid(self):
-        invalid_number_vatin = VATIN(VALID_VIES_COUNTRY_CODE, '12345678')
-        invalid_country_vatin = VATIN('XX', VALID_VIES_NUMBER)
-        validator = VATINValidator()
-
-        with pytest.raises(ValidationError):
-            validator(invalid_country_vatin)
-
-        validator = VATINValidator(verify=False, validate=True)
-        with pytest.raises(ValidationError):
-            validator(invalid_number_vatin)
-
-        validator = VATINValidator(verify=True, validate=True)
-        with pytest.raises(ValidationError):
-            validator(invalid_country_vatin)
-
-        with pytest.raises(ValidationError):
-            validator(invalid_number_vatin)
-
-
-class TestValidatorsWithoutVATINObject(object):
-    """Validate raw values without VATIN object from model field."""
-    def test_valid(self):
-        valid_vatin = ''.join([VALID_VIES_COUNTRY_CODE, VALID_VIES_NUMBER])
-        validator = VATINValidator()
-        validator(valid_vatin)
-
-        validator = VATINValidator(verify=False, validate=True)
-        validator(valid_vatin)
-
-        validator = VATINValidator(verify=True, validate=True)
-        validator(valid_vatin)
-
-    def test_invalid(self):
-        invalid_number_vatin = ''.join([VALID_VIES_COUNTRY_CODE, '12345678'])
-        invalid_country_vatin = ''.join(['XX', VALID_VIES_NUMBER])
-
-        validator = VATINValidator()
-        with pytest.raises(ValidationError):
-            validator(invalid_country_vatin)
-
-        validator = VATINValidator(verify=False, validate=True)
-        with pytest.raises(ValidationError):
-            validator(invalid_number_vatin)
-
-        validator = VATINValidator(verify=True, validate=True)
-        with pytest.raises(ValidationError):
-            validator(invalid_country_vatin)
-
-        with pytest.raises(ValidationError):
-            validator(invalid_number_vatin)
