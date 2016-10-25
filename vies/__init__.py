651ab97c98514608829ce671dd87d82e4de2af17
z0tomasz <tomek@tango.agency>
2016-10-25 16:43:39 +0200

    Disable logging
    Close #45
    Close #48

--- vies/__init__.py
+++ vies/__init__.py
@@ -3,12 +3,9 @@ from __future__ import (unicode_literals, absolute_import)
 
 import logging
 
-__version__ = "3.1.1"
+__version__ = "3.1.2"
 
 logger = logging.getLogger('vies')
-
-logging.basicConfig(level=logging.ERROR)
-logging.getLogger('suds.client').setLevel(logging.INFO)
 
 VIES_WSDL_URL = str('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')  # NoQA
 VATIN_MAX_LENGTH = 14
