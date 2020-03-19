83d4213877500fe127a013182fc44fca1dbc37a2
Manuel Domínguezgit@github.com:peillis/helpapp.git <manuel.dogo@gmail.com>
2020-03-19 18:11:44 +0100

    Fix vies url -- Increase version number

--- vies/__init__.py
+++ vies/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
 
-__version__ = "5.0.1"
+__version__ = "5.0.2"
 
 logger = logging.getLogger('vies')
 
-VIES_WSDL_URL = str('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')  # NoQA
+VIES_WSDL_URL = str('https://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')  # NoQA
 VATIN_MAX_LENGTH = 14
