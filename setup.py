2edb24b6d9f1be0b3fd9ebb17713734ccd82b197
Johannes Maron <info@johanneshoppe.com>
2021-07-14 09:49:05 +0200

    Fix #171 -- Include complied gettext msg in wheels

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import os
 import subprocess  # nosec
 from distutils.cmd import Command
 from distutils.command.build import build as _build
+from distutils.command.install import install as _install
 
 from setuptools import setup
-from setuptools.command.install_lib import install_lib as _install_lib
 
 BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 
@@ -17,19 +17,16 @@ class compile_translations(Command):
     user_options = []
 
     def initialize_options(self):
-        pass
+        self.build_lib = None
 
     def finalize_options(self):
-        pass
+        self.set_undefined_options("build", ("build_lib", "build_lib"))
 
     def run(self):
         pattern = "vies/locale/*/LC_MESSAGES/django.po"
         for file in glob.glob(pattern):
-            cmd = ["msgfmt", "-c"]
             name, ext = os.path.splitext(file)
-
-            cmd += ["-o", "%s.mo" % name]
-            cmd += ["%s.po" % name]
+            cmd = ["msgfmt", "-c", "-o", f"{self.build_lib}/{name}.mo", file]
             self.announce(
                 "running command: %s" % " ".join(cmd), level=distutils.log.INFO
             )
@@ -37,21 +34,24 @@ class compile_translations(Command):
 
 
 class build(_build):
-    sub_commands = [("compile_translations", None)] + _build.sub_commands
+    sub_commands = [
+        *_build.sub_commands,
+        ("compile_translations", None),
+    ]
 
 
-class install_lib(_install_lib):
-    def run(self):
-        self.run_command("compile_translations")
-        _install_lib.run(self)
-
+class install(_install):
+    sub_commands = [
+        *_install.sub_commands,
+        ("compile_translations", None),
+    ]
 
 setup(
     name="django-vies",
     use_scm_version=True,
     cmdclass={
         "build": build,
-        "install_lib": install_lib,
+        "install": install,
         "compile_translations": compile_translations,
     },
 )
